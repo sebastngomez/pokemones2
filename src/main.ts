7440e87c5de96e5675bd1ec35a32f74b983c407a
Raul Sebastian <rgomezbo@unal.edu.co>
2023-07-20 21:51:41 -0500

    Diploy to heroku

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(process.env.PORT);
+  await app.listen( process.env.PORT );
   console.log(`App runing on PORT ${ process.env.PORT } `);
   
 }
