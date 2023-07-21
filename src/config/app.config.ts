6e2eae688b0c74d7ef1686d45134ac0e62aeae52
Raul Sebastian <rgomezbo@unal.edu.co>
2023-07-21 14:56:00 -0500

    Este va a ser mi despliegue

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -6,6 +6,7 @@ export const EnvConfiguration = ()=>({
     enviroment: process.env.NODE_ENV || 'dev',
     mongodb: process.env.MONGODB,
     port: process.env.PORT || 3002,
-    default_limit: +process.env.DEFAULT_LIMIT || 7,
+    default_limit: +process.env.DEFAULT_LIMIT || 17,
 
 })
+
