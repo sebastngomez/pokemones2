d4a0ec99754531abaa559314d7479fe501fbc67c
Raul Sebastian <rgomezbo@unal.edu.co>
2023-07-22 18:58:35 -0500

    Ultimo despliegue a heroku por ahora

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -5,7 +5,7 @@ export const EnvConfiguration = ()=>({
 
     enviroment: process.env.NODE_ENV || 'dev',
     mongodb: process.env.MONGODB,
-    port: process.env.PORT,
+    port: process.env.PORT || 3000,
     default_limit: +process.env.DEFAULT_LIMIT || 17,
 
 })
--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,7 +2,7 @@
 import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
-
+    PORT:Joi.number().default(),
     MONGODB:Joi.required(),
     DEFAULT_LIMIT:Joi.number().default(8),
 })
