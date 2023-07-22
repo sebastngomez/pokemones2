25ecb4af6bb0a3c1107725184cbb29642bc7c8a7
Raul Sebastian <rgomezbo@unal.edu.co>
2023-07-22 18:30:58 -0500

    Ultimo despliegue a railways por ahora

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -4,8 +4,8 @@
 export const EnvConfiguration = ()=>({
 
     enviroment: process.env.NODE_ENV || 'dev',
-    // mongodb: process.env.MONGODB,
-    // port: process.env.PORT || 3000,
+    mongodb: process.env.MONGODB,
+    port: process.env.PORT || 3000,
     default_limit: +process.env.DEFAULT_LIMIT || 17,
 
 })
