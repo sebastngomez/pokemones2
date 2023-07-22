d3871f040dee0a204cddb329f298c95f62005f93
Raul Sebastian <rgomezbo@unal.edu.co>
2023-07-22 18:45:03 -0500

    Ultimo despliegue a railways por ahora

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,7 +2,7 @@
 import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
-    PORT:Joi.number().default(3005),
+
     MONGODB:Joi.required(),
     DEFAULT_LIMIT:Joi.number().default(8),
 })
